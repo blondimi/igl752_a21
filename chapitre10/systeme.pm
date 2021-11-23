d6552353124d15ec604aacdedc54e3b840f85e84
Michael Blondin <michael.blondin@tum.de>
2021-11-23 17:52:05 -0500

    Chap. 10

---
+++ chapitre10/systeme.pm
@@ -0,0 +1,30 @@
+// Chaîne de Markov
+dtmc
+
+const double p = 0.9;
+
+module systeme
+        // 0 = départ, 1 = envoyer, 2 = succès, 3 = échec
+	etat : [0..3] init 0;
+	
+	[] (etat = 0) -> (etat' = 1);
+	[] (etat = 1) -> p : (etat' = 2) + (1 - p) : (etat' = 3);
+	[] (etat = 2) -> (etat' = 0);
+	[] (etat = 3) -> (etat' = 1);
+endmodule
+
+// Incrémente le nombre d'envois chaque fois que «envoyer» est atteint
+rewards "nombre_envois"
+	(etat = 0 | etat = 3) : 1;
+	(etat = 1 | etat = 2) : 0;
+endrewards
+
+// Propositions atomiques
+label "envoyer" = (etat = 1);
+label "succes"  = (etat = 2);
+
+// Requêtes des notes de cours
+//  P>=1 [ F "succes" ]
+//  P>=1 [ G "envoyer" => P>=0.99 [ F<=3 "succes" ] ]
+//  P=?  [ F<=3 "succes" ]
+//  R{"nombre_envois"}=? [ F "succes" ]
